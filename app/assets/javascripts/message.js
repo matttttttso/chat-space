a4586814e9bf1c53c3aa545999a85e776f994d57
matttttttso <u2.pinetail@gmail.com>
2019-11-19 19:29:16 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,15 @@
 $(function(){
-  $(".input__submit--btn").on("click", function(e){
+  $('.form').on('submit', function(e){
     e.preventDefault()
-    console.log("イベント発火")
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,  
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
   })
 });
